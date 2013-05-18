8aa037cd1ac17161f83a1489643d64997ab1a072
Matt Swain <m.swain@me.com>
2013-05-18 16:34:44 +0100

    Fix analytics tracking for installing and upgrading

--- alexa.safariextension/js/global.js
+++ alexa.safariextension/js/global.js
@@ -6,9 +6,18 @@ var _gaq = _gaq || [];
 	ga.src = 'https://ssl.google-analytics.com/ga.js';
 	var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s);
 })();
-var previousVersion = safari.extension.settings.version;
-var currentVersion = '1.1'
-if (previousVersion != currentVersion) {
-	_gaq.push(['_trackEvent', currentVersion, previousVersion]);
-	safari.extension.settings.version = currentVersion;
-}
+
+(function() {
+	var prevVersion = safari.extension.settings.version;
+	switch (true) {
+	case (prevVersion == 111): // No upgrade
+		return;
+	case (prevVersion == '1.1'):
+	    _gaq.push(['_trackEvent', 'Install', '1.1']);
+		return;
+	case (prevVersion == 0): // New installation
+		_gaq.push(['_trackEvent', 'Install', 'New']);
+		break;
+	}
+	safari.extension.settings.version = 111;
+})()
